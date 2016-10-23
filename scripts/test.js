3d5a067866b212fac76751fc69a642470caab4a8
Ildiko Czeller <iczeller@emarsys.com>
2016-10-23 15:49:55 +0200

    Delete not working packages

--- scripts/test.js
+++
@@ -1,8 +0,0 @@
-function getToken() {
-  var token = '{"access_token":"' +
-    process.env.G_ACCESS_TOKEN +
-    '","token_type":"Bearer","refresh_token":"' +
-    process.env.G_REFRESH_TOKEN +
-    '","expiry_date":1477180321256}';
-  return JSON.parse(token);
-}
