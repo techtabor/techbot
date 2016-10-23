cd2558f9d06eab8eb7c626ae42c3ff2347749fe1
Ildiko Czeller <iczeller@emarsys.com>
2016-10-23 14:46:20 +0200

    test

--- scripts/user-test.js
+++ scripts/user-test.js
@@ -2,6 +2,11 @@
 //   me - echos your name
 module.exports = function(robot) {
     robot.hear(/me/i, function(msg){
-      msg.reply('hi there, ' + msg.message.user.name + msg.message.user.id);
+      msg.reply('hi there, ' +
+        msg.message.user.name +
+        " " + msg.message.user.id +
+        " " + msg.message.user.reply_to +
+        " " + msg.message.user.room
+    );
     });
 }
