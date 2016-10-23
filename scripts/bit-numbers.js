7b796e72b0a6e8c3a38622dd9c07b3e8099fe246
Ildiko Czeller <iczeller@emarsys.com>
2016-10-23 12:39:48 +0200

    try to find minimal not working example

--- scripts/bit-numbers.js
+++ scripts/bit-numbers.js
@@ -1,5 +1,5 @@
 var MsgReply = function () {
-  this.reply = "bit numbers under development";
+  this.reply = "";
 }
 
 MsgReply.prototype.getReply = function() {
@@ -18,7 +18,6 @@ module.exports = function(robot) {
 }
 
 var fs = require('fs');
-var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
@@ -36,15 +35,17 @@ function authorize(credentials, reply_with, msg, callback) {
   var redirectUrl = credentials.installed.redirect_uris[0];
   var auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  oauth2Client.setCredentials(JSON.parse(fs.readFileSync(TOKEN_PATH)));
+  callback(oauth2Client, reply_with, msg);
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client, reply_with, msg);
-    }
-  });
+  // fs.readFile(TOKEN_PATH, function(err, token) {
+  //   if (err) {
+  //     getNewToken(oauth2Client, callback);
+  //   } else {
+  //     oauth2Client.setCredentials(JSON.parse(token));
+  //     callback(oauth2Client, reply_with, msg);
+  //   }
+  // });
 }
 
 /**
@@ -74,7 +75,7 @@ function listBits(auth, reply_with, msg) {
       }
     }
     reply_with.setReply(reply);
-    msg.send("bit numbers under development\n"); // + reply_with.getReply());
+    msg.send("bit numbers under development\n"); //+ reply_with.getReply());
   });
 }
 
@@ -85,53 +86,4 @@ function getCredentials() {
     process.env.CLIENT_SECRET +
     '","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}';
   return JSON.parse(cs);
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
 }
