2eaa4071c1c44222cdb61f1a0b7c0188863bf9b9
Ildiko Czeller <iczeller@emarsys.com>
2016-10-23 15:03:01 +0200

    test reply to mentors only

--- scripts/bit-numbers.js
+++ scripts/bit-numbers.js
@@ -3,7 +3,7 @@
 module.exports = function(robot) {
     robot.hear(/bitek\? (.*)/i, function(msg){
       console.log(msg.message.user.name);
-      if (msg.message.user.name.match('czeildi|csirke|mdanka|nlaci|porcupine|szgabbor|vizilo') {
+      if (msg.message.user.name.match('csirke|mdanka|nlaci|porcupine|szgabbor|vizilo') {
         authorize(msg, msg.match[1], listBits);
       } else {
         msg.reply("Sorry, you are not allowed to ask everyone's bits. Use mybits? instead.")
