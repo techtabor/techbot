7ec8e048d50d637403cb81a0ff6511844507cac8
Ildiko Czeller <iczeller@emarsys.com>
2016-10-23 12:28:03 +0200

    restore test behavior

--- scripts/bit-numbers.js
+++ scripts/bit-numbers.js
@@ -13,11 +13,12 @@ MsgReply.prototype.setReply = function(r) {
 module.exports = function(robot) {
     robot.hear(/bitek\?/i, function(msg){
       reply_with = new MsgReply();
-      authorize(credentials(), reply_with, msg, listBits);
+      authorize(getCredentials(), reply_with, msg, listBits);
     });
 }
 
 var fs = require('fs');
+var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
@@ -35,7 +36,6 @@ function authorize(credentials, reply_with, msg, callback) {
   var redirectUrl = credentials.installed.redirect_uris[0];
   var auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, function(err, token) {
     if (err) {
@@ -48,14 +48,13 @@ function authorize(credentials, reply_with, msg, callback) {
 }
 
 /**
- * Print the names and number of bits of students in this spreadsheet:
- * https://docs.google.com/spreadsheets/d/14bKF4uSlKfjlgtP_t_ktQ4qLQzHfzu85zhWeix5uo5Q/edit#gid=1615643101
+ * Print the names and number of bits of students in the specified spreadsheet:
  */
 function listBits(auth, reply_with, msg) {
   var sheets = google.sheets('v4');
   sheets.spreadsheets.values.get({
     auth: auth,
-    spreadsheetId: '14bKF4uSlKfjlgtP_t_ktQ4qLQzHfzu85zhWeix5uo5Q',
+    spreadsheetId: process.env.HUBOT_GOOGLE_SPREADSHEET_SHEET_KEY,
     range: 'bitek!A2:B20',
   }, function(err, response) {
     var reply = "";
@@ -75,15 +74,64 @@ function listBits(auth, reply_with, msg) {
       }
     }
     reply_with.setReply(reply);
-    msg.send("bit numbers under development\n" + reply_with.getReply());
+    msg.send("bit numbers under development\n"); // + reply_with.getReply());
   });
 }
 
-function credentials() {
+function getCredentials() {
   var cs = '{"installed":{"client_id":"648831429316-0sm9tr096o2qtct9vn99om9466khlsp0.apps.googleusercontent.com",' +
     '"project_id":"angular-rhythm-147222","auth_uri":"https://accounts.google.com/o/oauth2/auth",' +
     '"token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"' +
     process.env.CLIENT_SECRET +
     '","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}';
   return JSON.parse(cs);
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
 }
