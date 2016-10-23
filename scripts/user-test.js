b590294c3a8d8a4204d8eefbd3deb2c3170bae96
Ildiko Czeller <iczeller@emarsys.com>
2016-10-23 14:22:42 +0200

    test user name functionality

--- scripts/user-test.js
+++ scripts/user-test.js
@@ -1,7 +1,7 @@
 // Commands:
 //   me? - echos your name
 module.exports = function(robot) {
-    robot.listen(/me\?/i, function(msg){
+    robot.hear(/me\?/i, function(msg){
       var name = msg.user.name;
       msg.reply(', you are #{name} to me.')
     });
