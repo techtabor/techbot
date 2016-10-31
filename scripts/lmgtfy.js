5e50e865a36438b89df4313e939a0d118a44a314
Ildiko Czeller <iczeller@emarsys.com>
2016-10-31 14:47:15 +0100

    do not use backtick

--- scripts/lmgtfy.js
+++ scripts/lmgtfy.js
@@ -2,8 +2,7 @@
 //   google <query> - returns query of lmgtfy
 module.exports = function(robot) {
     robot.respond(/google (.*)/i, function (msg) {
-        var question = msg.match[1]
-        msg.send(`http://lmgtfy.com/?q=${question}`);
+        msg.send('http://lmgtfy.com/?q=' + msg.match[1]);
     }
 );
 }
