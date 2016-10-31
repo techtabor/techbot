c00c6473d652f144de06e8c2bb448a0a92cddeba
marci07iq <marci07iq@outlook.com>
2016-10-31 22:10:30 +0100

    Fix for all characters

--- scripts/lmgtfy.js
+++ scripts/lmgtfy.js
@@ -2,7 +2,7 @@
 //   google <query> - returns query of lmgtfy
 module.exports = function(robot) {
     robot.hear(/google (.*)/i, function (msg) {
-        msg.reply('http://lmgtfy.com/?q=' + msg.match[1].split(' ').join('+'))
+        msg.reply('http://lmgtfy.com/?q=' + encodeURIComponent(msg.match[1]))
     }
 );
 }
