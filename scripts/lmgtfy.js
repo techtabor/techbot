27770c6016560b1c7a7afca60083db2568cac866
marci07iq <marci07iq@outlook.com>
2016-10-31 22:16:46 +0100

    del modified version

--- scripts/lmgtfy.js
+++
@@ -1,8 +0,0 @@
-// Commands:
-//   google <query> - returns query of lmgtfy
-module.exports = function(robot) {
-    robot.hear(/google (.*)/i, function (msg) {
-        msg.reply('http://lmgtfy.com/?q=' + encodeURIComponent(msg.match[1]))
-    }
-);
-}
