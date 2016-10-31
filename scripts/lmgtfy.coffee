fde93ff3d608305c1de21fcc1956f8f8f224c76b
Ildiko Czeller <iczeller@emarsys.com>
2016-10-31 15:03:34 +0100

    fix lmgtfy doc

--- scripts/lmgtfy.coffee
+++
@@ -1,6 +0,0 @@
-# Commands:
-#   google <query> - returns query of lmgtfy
-# module.exports = (robot) ->
-#   robot.respond /google (.*) /i, (res) ->
-#  	  question = res.match[1]
-#     res.reply "http://lmgtfy.com/?q=#{question}"
--- scripts/next.js
+++ scripts/next.js
@@ -1,5 +1,5 @@
 // Commands:
-//   next? - tells the time of the next tecttabor
+//   next? - tells the time of the next techtabor
 module.exports = function(robot) {
     robot.hear(/next\?/i, function (msg) {
         msg.send("2016. November 14.");
