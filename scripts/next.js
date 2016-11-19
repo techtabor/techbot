7a6f6f58781d5f8f6c7b013413f6bc166f52929a
Ildiko Czeller <czeildi@gmail.com>
2016-11-19 23:54:18 +0100

    change next techtabor date

--- scripts/next.js
+++ scripts/next.js
@@ -2,7 +2,7 @@
 //   next? - tells the time of the next techtabor
 module.exports = function(robot) {
     robot.hear(/next\?/i, function (msg) {
-        msg.send("2016. November 14.");
+        msg.send("2016. December 21.");
     }
 );
 }
