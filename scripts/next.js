c171b603048de9067b41d3d4e757ec89d48e24d4
Czeller Ildikó <czeildi@users.noreply.github.com>
2017-06-05 12:07:45 +0200

    Update next.js

--- scripts/next.js
+++ scripts/next.js
@@ -2,7 +2,7 @@
 //   next? - tells the time of the next techtabor
 module.exports = function(robot) {
     robot.hear(/next\?/i, function (msg) {
-        msg.send("2017. Január 30/31.");
+        msg.send("2017. Június 5-6.: záróalkalom. :)");
     }
 );
 }
